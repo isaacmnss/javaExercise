9fc9245b98a070a50f7627e9babbb8068f18b849
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-08-04 02:06:49 -0300

    Add ex003

--- src/Ex003.java
+++ src/Ex003.java
@@ -1,3 +1,7 @@
+//Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais,
+// deverá somar os dois valores,caso contrário devera multiplicar A por B. Ao final de qualquer
+// um dos cálculos deve-se atribuir o resultado a uma variável C imprimir seu valor na tela.
+
 import java.util.Scanner;
 
 public class Ex003 {
