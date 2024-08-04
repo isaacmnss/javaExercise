d2ebf769df245d1544935aefeb58ed71c6fa90cc
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-08-04 02:07:48 -0300

    Add ex002

--- src/Ex002.java
+++ src/Ex002.java
@@ -1,3 +1,6 @@
+// 2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar,
+// positivo ou negativo.
+
 import java.util.Scanner;
 
 public class Ex002 {
